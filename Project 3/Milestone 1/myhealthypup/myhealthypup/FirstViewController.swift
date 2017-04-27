200a3c3d227bdfdef58975045870bdde3a219b9e
Fuji Robledo <ProgrammingPenguin@Fujis-MacBook-Pro.local>
2017-04-26 18:29:30 -0600

    Milestone 1 FINAL

---
+++ Project 3/Milestone 1/myhealthypup/myhealthypup/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  myhealthypup
+//
+//  Created by Fuji Robledo on 4/26/17.
+//  Copyright © 2017 Fuji Robledo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
